b61be8bcee38b310df8f2920da36ccf2927fcc49
samiksha99 <agrawalsamiksha25@gmail.com>
2021-01-30 12:10:40 +0530

    flutter setup

---
+++ android/app/src/main/kotlin/com/example/decentagram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.decentagram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Decentagram',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue,
+      primaryColorBrightness: Brightness.light,
+      visualDensity:VisualDensity.adaptivePlatformDensity
+      ),
+      home: MyHomePage(title: 'Decentagram'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  Widget buildBottomIconButton(IconData icon,Color color)
+  {
+    return IconButton(
+      icon:Icon(
+        icon,
+        color: color,
+      ),
+      onPressed: () {},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: Container(
+          margin: EdgeInsets.all(10.0),
+          child: CircleAvatar(
+            backgroundImage:AssetImage('assets/images/dp.jpeg'),
+          ),
+        ),
+        title: Text(
+          'Home',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight:FontWeight.bold,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            buildBottomIconButton(Icons.home, Colors.blue),
+            buildBottomIconButton(Icons.search, Colors.black45),
+            buildBottomIconButton(Icons.notifications, Colors.black45),
+            buildBottomIconButton(Icons.mail_outline, Colors.black45),
+          ],
+        ),
+      ),
+    );
+  }
+}
