83af14377e4154ed72ff1dd917733dc4987ea48f
kahkasha123 <kahkasha987@gmail.com>
2021-01-31 09:22:02 +0530

    Contract upadate

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final myAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";
+
   
   Widget buildBottomIconButton(IconData icon,Color color)
   {
